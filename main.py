4f1080dd23f315d4222d1a4adc04f98d81ea97a6
Alan Cleary <alan.cleary@msu.montana.edu>
2017-04-02 16:49:00 -0600

    Completed TG-34 #ready-for-test

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 if __name__ == '__main__':
-    pass
+
+    import sys
+    from package import app
+    sys.exit(app.run())
---
+++ package/app.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+
+def run():
+    app = QApplication(sys.argv)
+    return app.exec_()
