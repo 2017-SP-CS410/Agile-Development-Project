fe7f9cff7d8653d5fd5c44433815261bb521af80
Asher Holloman <asherholloman@gmail.com>
2017-04-03 16:37:45 -0600

    music player

---
+++ package/musicPlayer.py
@@ -0,0 +1,10 @@
+from PyQt5.QtGui     import *
+from PyQt5.QtOpenGL  import QGLWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore    import *
+from textwrap        import dedent
+from PyQt5.QtMultimedia   import QAudio, QAudioDeviceInfo, QAudioFormat, QAudioOutput
+
+
+while True:
+    print("hello")
