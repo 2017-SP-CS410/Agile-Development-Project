725b00e372b89a0acca2073f6459d4e1ec613534
Alan Cleary <alan.cleary@msu.montana.edu>
2017-04-03 20:58:38 -0600

    Completed TG-23 TG-24 TG-25 #ready-for-test

--- package/ui/widgets/gamewidget.py
+++ package/ui/widgets/gamewidget.py
@@ -1,28 +1,183 @@
+from array          import array
+from ctypes         import c_void_p
 from textwrap       import dedent
 from OpenGL.GL      import *
 from OpenGL.GLU     import *
 from PyQt5.QtOpenGL import QGLWidget
+from PyQt5.QtGui    import QImage, QMatrix4x4, qRgb, QVector3D
 
 
 class GameWidget(QGLWidget):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, n=10, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.setMinimumSize(640, 480)
+        self.n = n
+        self.restart = 0xFFFFFFFF
 
 
-    def initializeGL(self):
+    def initializeCube(self):
+
+        self.vertices = array('f')
+        self.colors = array('f')
+        self.indices = array('I')
+
+        half_n = self.n / 2
+        x = y = -half_n
+        i = 0
+        while x < half_n:
+            while y < half_n:
+                self.vertices.extend([
+                    # +y face
+                    x+1, y+1,  0,  # 0
+                    x+1, y+1, -1,  # 1
+                      x, y+1, -1,  # 2
+                      x, y+1,  0,  # 3
+                    # -y face
+                    x+1, y,  0,  # 4
+                      x, y,  0,  # 5
+                      x, y, -1,  # 6
+                    x+1, y, -1,  # 7
+                    # top
+                    x+1, y+1, 0,  # 8
+                      x, y+1, 0,  # 9
+                      x,   y, 0,  # 10
+                    x+1,   y, 0,  # 11
+                    # bottom
+                      x,   y, -1,  # 12
+                      x, y+1, -1,  # 13
+                    x+1, y+1, -1,  # 14
+                    x+1,   y, -1,  # 15
+                    # +x face
+                    x+1,   y,  0,  # 16
+                    x+1,   y, -1,  # 17
+                    x+1, y+1, -1,  # 18
+                    x+1, y+1,  0,  # 19
+                    # -x face
+                    x,   y,  0,  # 20
+                    x, y+1,  0,  # 21
+                    x, y+1, -1,  # 22
+                    x,   y, -1   # 23
+                ])
+                self.colors.extend([
+                    # top
+                    0,1,0,    
+                    0,1,0,    
+                    0,1,0,    
+                    0,1,0,    
+                    # bottom
+                    0,.5,0,  
+                    0,.5,0,  
+                    0,.5,0,  
+                    0,.5,0,  
+                    # front
+                    0,0,1,    
+                    0,0,1,    
+                    0,0,1,    
+                    0,0,1,    
+                    # back
+                    0,0,.5,  
+                    0,0,.5,  
+                    0,0,.5,  
+                    0,0,.5,
+                    # right
+                    1,0,0,    
+                    1,0,0,    
+                    1,0,0,    
+                    1,0,0,    
+                    # left
+                    .5,0,0,  
+                    .5,0,0,  
+                    .5,0,0,  
+                    .5,0,0  
+                ])
+                self.indices.extend([
+                     i+0,  i+1,  i+2,  i+3, self.restart,
+                     i+4,  i+5,  i+6,  i+7, self.restart,
+                     i+8,  i+9, i+10, i+11, self.restart,
+                    i+12, i+13, i+14, i+15, self.restart,
+                    i+16, i+17, i+18, i+19, self.restart,
+                    i+20, i+21, i+22, i+23, self.restart
+                ])
+                y += 1
+                i += 24
+            y = -half_n
+            x += 1
+        self.indices.pop()
+
+        # create a new Vertex Array Object on the GPU which saves the attribute
+        # layout of our vertices
+        self.cubeVao = glGenVertexArrays(1)
+        glBindVertexArray(self.cubeVao)
+
+        # create a buffer on the GPU for position and color data
+        vertexBuffer = glGenBuffers(1)
+        colorBuffer = glGenBuffers(1)
+        indexBuffer = glGenBuffers(1)
+
+        # upload the data to the GPU, storing it in the buffer we just created
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer)
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            self.sizeof(self.vertices),
+            self.vertices.tostring(),
+            GL_STATIC_DRAW
+        )
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer)
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            self.sizeof(self.colors),
+            self.colors.tostring(),
+            GL_STATIC_DRAW
+        )
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer)
+        glBufferData(
+            GL_ELEMENT_ARRAY_BUFFER,
+            self.sizeof(self.indices),
+            self.indices.tostring(),
+            GL_STATIC_DRAW
+        )
+
         # load our vertex and fragment shaders into a program object on the GPU
         program = self.loadShaders()
+        glUseProgram(program)
+        self.cubeProg = program
 
         # bind the attribute "position" (defined in our vertex shader) to the
         # currently bound buffer object, which contains our position data
         # this information is stored in our vertex array object
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer)
         position = glGetAttribLocation(program, 'position')
         glEnableVertexAttribArray(position)
-
-        # bind the attribute "color" to the buffer object
+        glVertexAttribPointer(
+            position,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            c_void_p(0)
+        )
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer)
         color = glGetAttribLocation(program, 'color')
         glEnableVertexAttribArray(color)
+        glVertexAttribPointer(
+            color,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            c_void_p(0)
+        )
+
+        self.cubeProjMatLoc = glGetUniformLocation(program, "projection")
+
+
+    def initializeGL(self):
+        glEnable(GL_DEPTH_TEST)
+        glPrimitiveRestartIndex(self.restart)
+        glEnable(GL_PRIMITIVE_RESTART)
+        self.initializeCube()
+
 
 
     def loadShaders(self):
@@ -32,10 +187,14 @@ class GameWidget(QGLWidget):
         # vertex shader
         vs_source = dedent("""
             #version 330
+            uniform mat4 projection;
             in vec3 position;
+            in vec3 color;
+            out vec3 fcolor;
             void main()
             {
-               gl_Position = vec4(position, 1.0);
+               gl_Position = projection * vec4(position, 1.0);
+               fcolor = color;
             }\
         """)
         vs = glCreateShader(GL_VERTEX_SHADER)
@@ -48,9 +207,10 @@ class GameWidget(QGLWidget):
         # fragment shader
         fs_source = dedent("""
             #version 330
+            in vec3 fcolor;
             void main()
             {
-               gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+               gl_FragColor = vec4(fcolor, 1.0);
             }\
         """)
         fs = glCreateShader(GL_FRAGMENT_SHADER)
@@ -69,7 +229,36 @@ class GameWidget(QGLWidget):
 
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        #glDrawArrays(GL_TRIANGLE_FAN, 0, len(self.vertices))
+        self.renderCube()
+
+
+    def renderCube(self):
+        glUseProgram(self.cubeProg)
+        glBindVertexArray(self.cubeVao)
+        glDrawElements(
+            GL_TRIANGLE_FAN,
+            len(self.indices),
+            GL_UNSIGNED_INT,
+            c_void_p(0)
+        )
 
 
     def resizeGL(self, width, height):
         glViewport(0, 0, width, height)
+
+        camera = QMatrix4x4()
+        camera.perspective(60, 4.0/3.0, 0.1, 100.0)
+        camera.lookAt(QVector3D(10, 10, 10), QVector3D(0, 0, 0), QVector3D(0, 0, 1))
+
+        glUseProgram(self.cubeProg)
+        glUniformMatrix4fv(
+            self.cubeProjMatLoc,
+            1,
+            GL_FALSE,
+            array('f', camera.data()).tostring()
+        )
+
+
+    def sizeof(self, a):
+        return a.itemsize * len(a)
