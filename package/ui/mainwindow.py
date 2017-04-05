bb75030b296f0c5402ff83561a61f3940a81afb5
MystiriodisLykos <MystiriodisLykos@gmail.com>
2017-04-05 13:32:01 -0600

    Completed TG-17 #ready-for-test
    Added WASD key events that change a sudo character objects x,y positions

--- package/ui/mainwindow.py
+++ package/ui/mainwindow.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets    import QMainWindow, QStackedWidget
 from package.ui.widgets import GameWidget
 
+character = {'x': 0, 'y': 0, 'speed': 1}
+
 
 class MainWindow(QMainWindow):
 
@@ -9,3 +11,16 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Game name here')
         #self.stacked_widget = QStackedWidget(GameWidget())
         self.setCentralWidget(GameWidget())
+
+    def keyPressEvent(self, qKeyEvent):
+        key = qKeyEvent.key()
+        if key == ord('w')-32:
+            character['y'] += character['speed']
+        elif key == ord('a')-32:
+            character['x'] -= character['speed']
+        elif key == ord('s')-32:
+            character['y'] -= character['speed']
+        elif key == ord('d')-32:
+            character['x'] += character['speed']
+        elif key == ord(' ')-32:
+            print(' ')
